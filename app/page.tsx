23e7de470d2b6b13725d7df8d4f8eeeb5f6b358a
Tushar Velingkar <Tushar.Velingkar@ibm.com>
2025-02-06 19:52:50 +0530

    Initital page changes

--- app/page.tsx
+++ app/page.tsx
@@ -13,12 +13,6 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
         <div className={styles.ctas}>
           <a
